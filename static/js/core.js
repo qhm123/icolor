e2e8e616223e59a17938fdffdb7d583a024ac1c1
鸣 <qhm123@gmail.com>
2012-01-31 09:28:30 +0800

    clean up

--- static/js/core.js
+++
@@ -1,9 +0,0 @@
-function randomChar(l){
-	var x = "0123456789abcdef";
-	var tmp = "";
-	for(var i = 0; i < l; i++){
-		tmp += x.charAt(Math.ceil(Math.random() * 100000000) % x.length);
-	}
-	
-	return tmp;
-}
